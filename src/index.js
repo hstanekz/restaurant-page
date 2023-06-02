451f10ffd092e9cc9783f9aa660986772b5237f0
Hiwad Stanekzai <Hiwad_stanekzai@yahoo.com>
2023-06-01 17:53:33 -0700

    Add imports for navbar, main-home, footer, main-menu, and about modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import { renderNav } from "./navbar";
+import { renderHome } from "./main-home";
+import { renderFooter } from "./footer";
+import { renderMenu } from "./main-menu";
+import { renderAbout } from "./about";
+
 const content = document.getElementById("content");
 
 function createHtmlElement(type, id, arrayClasses, content) {
@@ -10,3 +16,33 @@ function createHtmlElement(type, id, arrayClasses, content) {
 
   return element;
 }
+
+function home() {
+    content.innerHTML = "";
+    renderNav();
+    renderHome();
+    renderFooter();
+  }
+  function menu() {
+    content.innerHTML = "";
+    renderNav();
+    renderMenu();
+    renderFooter();
+  }
+  function about() {
+    content.innerHTML = "";
+    renderNav();
+    renderAbout();
+    renderFooter();
+  }
+  
+  home();
+  document.addEventListener("click", (e) => {
+    const target = e.target.innerText;
+  
+    if (target === "HOME") home();
+    if (target === "MENU" || target === "SEE MENU") menu();
+    if (target === "ABOUT") about();
+  });
+
+export { createHtmlElement, content };
