e8c32ea1ca5e145ac6e2b4f8780a904a901c9fd3
Hiwad Stanekzai <Hiwad_stanekzai@yahoo.com>
2023-06-01 17:49:25 -0700

    Create about me module and append to main

---
+++ src/about.js
@@ -0,0 +1,24 @@
+import { content, createHtmlElement } from "./index";
+
+function renderAbout() {
+  const main = document.createElement("main");
+  main.classList.add("main");
+
+  const welcome = createHtmlElement("p", null, ["cursive", "golden"], "Welcome");
+  const title = createHtmlElement("h1", null, ["gray"], "About Us");
+
+  const hr = document.createElement("hr");
+
+  const content =
+    "At our pizza restaurant, we strive to deliver the finest pizzas made with the freshest ingredients. Our commitment to quality and taste ensures a delightful experience for our customers. Vivamus sit amet porta mauris, in vestibulum ante. Phasellus nec nisi eros. Quisque ultricies urna sit amet neque posuere luctus. Etiam viverra magna ac ligula pellentesque, sed hendrerit felis pulvinar."
+  const p = createHtmlElement("p", null, ["menu-item"], content);
+
+  main.appendChild(welcome);
+  main.appendChild(title);
+  main.appendChild(hr);
+  main.appendChild(p);
+
+  content.appendChild(main);
+}
+
+export { renderAbout };
