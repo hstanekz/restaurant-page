0a0e4adf8f4e3d31cbc6d65235dbdc04dee2d85d
Hiwad Stanekzai <Hiwad_stanekzai@yahoo.com>
2023-06-01 17:46:13 -0700

    Append the rendered footer to the content element

---
+++ src/footer.js
@@ -0,0 +1,14 @@
+import { content, createHtmlElement } from "./index.js";
+
+function render() {
+  const footer = document.createElement("footer");
+  const p = createHtmlElement(
+    "p",
+    null,
+    ["golden"]
+  );
+  footer.appendChild(p);
+  $content.appendChild(footer);
+}
+
+export { render as renderFooter };
