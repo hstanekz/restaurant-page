d01c9ec5d8064d7e7c1250377bd1b9b509f8e34c
Hiwad Stanekzai <Hiwad_stanekzai@yahoo.com>
2023-06-01 17:31:15 -0700

    Define createHtmlElement function to simplify HTML element creation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import {nav} from './navbar.js';
+const content = document.getElementById("content");
 
-nav();
+function createHtmlElement(type, id, arrayClasses, content) {
+  const element = document.createElement(type);
+  if (id) element.id = id;
+  if (arrayClasses)
+    arrayClasses.forEach((myClass) => element.classList.add(myClass));
+
+  if (content) element.innerText = content;
+
+  return element;
+}
